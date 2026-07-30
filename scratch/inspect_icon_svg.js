c5938c9b9789794b6e31758714b8782339c5fbfd
Trisage Marketing Company <trisagemarketing@gmail.com>
2026-07-30 11:51:21 +0530

    Final Google-compliant transparent emblem favicon without text

--- scratch/inspect_icon_svg.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const svg = fs.readFileSync('public/icon.svg', 'utf8');
-const cleanSvg = svg.replace(/data:image\/png;base64,[^"']+/g, '[BASE64_DATA]');
-console.log(cleanSvg);
--- scratch/test_sharp_module.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-try {
-  const sharp = require('sharp');
-  console.log('sharp module is directly available!');
-} catch(e) {
-  console.log('sharp module not directly in project node_modules:', e.message);
-}
