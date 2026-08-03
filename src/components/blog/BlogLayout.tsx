3c1cf83ad1b09dd9a649fd31d2c6116dbbe4cd62
Trisage Marketing Company <trisagemarketing@gmail.com>
2026-08-03 19:30:18 +0530

    style(blog): completely hide the category filter pills section

--- src/components/blog/BlogLayout.tsx
+++ src/components/blog/BlogLayout.tsx
@@ -80,7 +80,7 @@ export default function BlogLayout({
       </section>
 
       {/* Category Filter Pills */}
-      <section className="container mx-auto px-4 md:px-8 pb-8 sm:pb-12 z-10 relative">
+      <section className="hidden container mx-auto px-4 md:px-8 pb-8 sm:pb-12 z-10 relative">
         <div className="flex flex-wrap items-center justify-center gap-2 md:gap-3 py-2">
           {categories.map((category) => (
             <button
